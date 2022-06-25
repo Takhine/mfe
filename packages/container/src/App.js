87b3adf136bdaef57b618c66fb6158b36c7d75e6
Aniketh Nair <anikethnair.developer@gmail.com>
2022-06-26 00:04:08 +0530

    Dashboard mfe

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,31 +1,48 @@
-import React, { useState, lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { useState, lazy, Suspense, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+import { createBrowserHistory } from "history";
 import Header from "./components/Header";
 import Progress from "./components/Progress";
 
 const Marketing = lazy(() => import("./components/MarketingApp"));
 const Auth = lazy(() => import("./components/AuthApp"));
+const Dashboard = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <div>
-            <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+            <Header
+              isSignedIn={isSignedIn}
+              onSignOut={() => setIsSignedIn(false)}
+            />
             <Suspense fallback={<Progress />}>
               <Switch>
                 <Route path="/auth">
                   <Auth onSignIn={() => setIsSignedIn(true)} />
+                </Route>
+                <Route path="/dashboard">
+                  {!isSignedIn && <Redirect to="/" />}
+                  <Dashboard />
                 </Route>
                 <Route path="/" component={Marketing}>
                   <Marketing />
@@ -33,7 +50,7 @@ export default () => {
               </Switch>
             </Suspense>
           </div>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
